c04b6a47b02ec287ce19203cb61ee7624a6c0ed4
izzahfatini <izzahfatini@graduate.utm.my>
2023-06-26 00:10:26 +0800

    commit

---
+++ src/main/java/com/example/eiger/EigerApplication.java
@@ -0,0 +1,13 @@
+package com.example.eiger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EigerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EigerApplication.class, args);
+	}
+
+}
